23f3e134bb37a1b33e1eeec6ce5727aa43b30820
bobrynja <119498240+bobrynja@users.noreply.github.com>
2022-12-19 14:25:02 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        class wDate
+        {
+            private DateTime data;
+
+            public wDate(string dt)
+            {
+                string[] n = dt.Split('.');
+                data = new DateTime (Convert.ToInt32(n[2]), Convert.ToInt32(n[1]), Convert.ToInt32(n[0]));
+            }
+            public wDate()
+            {
+                data = new DateTime(2009, 01, 01);
+            }
+            public string priviousDay()
+            {
+                return data.AddDays(-1).ToShortDateString();
+            }
+            public string nextDay()
+            {
+                return data.AddDays(1).ToShortDateString();
+            }
+            public int remainDays()
+            {
+
+                int month = data.Month+1;
+                int year = data.Year;
+                if (month == 13)
+                {
+                    month = 1;
+                    year = data.Year + 1;
+                }
+                
+                DateTime border = new DateTime(year, month, 01);
+                border = border.AddDays(-1);
+                return border.Day - data.Day;
+            }
+
+            //Свойства:•	позволяющее установить или получить значение поле класса (доступно для чтения и записи)
+            public DateTime Data
+            {
+                set { data = value; }
+                get { return data; }
+            }
+
+            //позволяющее определить год высокосным (доступно только для чтения)
+            public string IsLeap
+            {
+                get {
+                    Boolean dt = DateTime.IsLeapYear(data.Year);
+                    string res = "Нет";
+                    if (dt) res = "Да";
+                    return res;
+                     }
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите дату в формает дд.мм.гггг: ");
+                wDate date = new wDate(Console.ReadLine());
+
+                Console.WriteLine("Дата предыдущего дня: {0}", date.priviousDay());
+
+                Console.WriteLine("Дата следующего дня: {0}", date.nextDay());
+                Console.WriteLine("Количество дней до конца месяца: {0}", date.remainDays());
+                Console.WriteLine("Текущая дата: " + date.Data.ToShortDateString());
+                Console.Write("Вы хотите изменить дату? ");
+                string answer = Console.ReadLine();
+                if (answer == "Да" || answer == "да")
+                {
+                    Console.Write("Введите дату в формате дд.мм.гггг: ");
+                    string[] n = Console.ReadLine().Split('.');
+                    DateTime newdate = new DateTime(Convert.ToInt32(n[2]), Convert.ToInt32(n[1]), Convert.ToInt32(n[0]));
+                    date.Data = newdate;
+                }
+
+
+                Console.WriteLine("Является ли год выскокосным? {0}", date.IsLeap);
+            }
+            catch { Console.WriteLine("Некорректный ввод данных!!!"); }
+                Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        class wDate
+        {
+            private DateTime data;
+
+            public wDate(string dt)
+            {
+                string[] n = dt.Split('.');
+                data = new DateTime(Convert.ToInt32(n[2]), Convert.ToInt32(n[1]), Convert.ToInt32(n[0]));
+            }
+            public wDate()
+            {
+                data = new DateTime(2009, 01, 01);
+            }
+            public string priviousDay()
+            {
+                return data.AddDays(-1).ToShortDateString();
+            }
+            public string nextDay()
+            {
+                return data.AddDays(1).ToShortDateString();
+            }
+            public int remainDays()
+            {
+
+                int month = data.Month + 1;
+                int year = data.Year;
+                if (month == 13)
+                {
+                    month = 1;
+                    year = data.Year + 1;
+                }
+
+                DateTime border = new DateTime(year, month, 01);
+                border = border.AddDays(-1);
+                return border.Day - data.Day;
+            }
+
+            
+            public DateTime Data
+            {
+                set { data = value; }
+                get { return data; }
+            }
+
+            
+            public string IsLeap
+            {
+                get
+                {
+                    Boolean dt = DateTime.IsLeapYear(data.Year);
+                    string res = "Нет";
+                    if (dt) res = "Да";
+                    return res;
+                }
+            }
+
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                wDate date = new wDate(textBox1.Text);
+                textBox2.Text = "Информация:" + Environment.NewLine;
+                textBox2.Text += "Дата предыдущего дня: " + date.priviousDay() + Environment.NewLine;
+
+                textBox2.Text += "Дата следующего дня: " + date.nextDay() + Environment.NewLine;
+                textBox2.Text += "Количество дней до конца месяца: " + date.remainDays() + Environment.NewLine;
+                textBox2.Text += "Текущая дата: " + date.Data.ToShortDateString() + Environment.NewLine;
+                textBox2.Text += "Является ли год выскокосным? " + date.IsLeap + Environment.NewLine;
+            }
+            catch {MessageBox.Show("Некорректный ввод данных!!!"); }
+        }
+
+        
+    }
+}
